45ee6a69b68c7c59fea35c32660e02263dbb7154
CBush88 <christopherbush88@gmail.com>
2022-10-12 14:08:46 -0400

    Spring Boot Initializr

---
+++ client-project-case-study-api/src/main/java/com/genspark/clientprojectcasestudy/ClientProjectCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.genspark.clientprojectcasestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientProjectCaseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientProjectCaseStudyApplication.class, args);
+	}
+
+}
---
+++ client-project-case-study-api/src/test/java/com/genspark/clientprojectcasestudy/ClientProjectCaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genspark.clientprojectcasestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientProjectCaseStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
