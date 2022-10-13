b449c282f23508702755bc9eade26bc368bd011f
CBush88 <christopherbush88@gmail.com>
2022-10-13 16:48:49 -0400

    Implemented routing and retrieving project list

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import './App.css';
 import Clients from './components/Clients';
 import { getClients } from './services/ClientsData';
+import { getProjects } from './services/ProjectsData';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Projects from './components/Projects';
 
 function App() {
 
@@ -15,10 +18,25 @@ function App() {
     .catch(err => console.log(err.response))
   }
 
+  const [projects, setProjects] = useState([])
+
+  const retrieveProjects = () => {
+    getProjects()
+    .then(res => {
+      setProjects(res.data)
+    })
+    .catch(err => console.log(err.response))
+  }
+
   return (
     <div className="App">
       <div className='container'>
-      <Clients clients={clients} setClients={setClients} retrieveClients={retrieveClients} />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Clients clients={clients} setClients={setClients} retrieveClients={retrieveClients} />} />
+          <Route path='/projects' element={<Projects projects={projects} setProjects={setProjects} retrieveProjects={retrieveProjects} />} />
+        </Routes>
+      </Router>
       </div>
     </div>
   );
