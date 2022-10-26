e1eba7d35d28c4f6ac90951332947bf0f4101db0
CBush88 <christopherbush88@gmail.com>
2022-10-26 09:35:28 -0400

    Added repository annotation

--- client-project-case-study-api/src/main/java/com/genspark/clientprojectcasestudy/Dao/ClientDao.java
+++ client-project-case-study-api/src/main/java/com/genspark/clientprojectcasestudy/Dao/ClientDao.java
@@ -2,6 +2,8 @@ package com.genspark.clientprojectcasestudy.Dao;
 
 import com.genspark.clientprojectcasestudy.Entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientDao extends JpaRepository<Client, Integer> {
 }
--- client-project-case-study-api/src/main/java/com/genspark/clientprojectcasestudy/Dao/ProjectDao.java
+++ client-project-case-study-api/src/main/java/com/genspark/clientprojectcasestudy/Dao/ProjectDao.java
@@ -2,6 +2,8 @@ package com.genspark.clientprojectcasestudy.Dao;
 
 import com.genspark.clientprojectcasestudy.Entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProjectDao extends JpaRepository<Project, Integer> {
 }
